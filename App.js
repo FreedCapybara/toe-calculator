07e7794fb8982822a94fc8da50e464fce9f049d5
atemple <templeandrew@outlook.com>
2020-07-05 23:16:53 -0400

    It is now perfect

--- App.js
+++ App.js
@@ -12,21 +12,39 @@ export default function PizzaTranslator() {
 
   return (
     <View style={{padding: 10}}>
+      <Text>Rear</Text>
       <TextInput
-        style={{height: 40}}
-        placeholder="Enter the rear measurement"
+        style={{padding: 4, height: 40, borderColor: '#e0e0e0', borderWidth: 1}}
+        placeholder="Enter the rear measurement (decimal inches)"
         onChangeText={rear => setRear(rear)}
         defaultValue={rear}
       />
+      <Text>Front</Text>
       <TextInput
-        style={{height: 40}}
-        placeholder="Enter the front measurement"
+        style={{padding: 4, height: 40, borderColor: '#e0e0e0', borderWidth: 1}}
+        placeholder="Enter the front measurement (decimal inches)"
         onChangeText={front => setFront(front)}
         defaultValue={front}
       />
       <Text style={{padding: 10, fontSize: 42}}>
-        {getToe(front, rear).toFixed(2)}
+        {getToe(front, rear).toFixed(2)}&deg;
       </Text>
+      <Text style={{fontStyle: 'italic'}}>Cheatsheet:</Text>
+      <Text>1/16 = .0625</Text>
+      <Text>1/8 = .125</Text>
+      <Text>3/16 = .1875</Text>
+      <Text>1/4 = .25</Text>
+      <Text>5/16 = .3125</Text>
+      <Text>3/8 = .375</Text>
+      <Text>7/16 = .4375</Text>
+      <Text>1/2 = .5</Text>
+      <Text>9/16 = .5625</Text>
+      <Text>5/8 = .625</Text>
+      <Text>11/16 = .6875</Text>
+      <Text>3/4 = .75</Text>
+      <Text>13/16 = .8125</Text>
+      <Text>7/8 = .875</Text>
+      <Text>15/16 = .9375</Text>
     </View>
   );
 }
